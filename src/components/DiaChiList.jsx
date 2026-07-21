08722666f995ddd042f141778ab9cbb5225e9b72
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:01:44 +0700

    Update DiaChiList.jsx

---
+++ src/components/DiaChiList.jsx
@@ -0,0 +1,21 @@
+import diaChiDo from "../data/diaChiDo";
+import DiaChiCard from "./DiaChiCard";
+
+function DiaChiList() {
+  return (
+    <section className="diachi">
+      <h2>📍 ĐỊA CHỈ ĐỎ NỔI BẬT</h2>
+
+      <div className="diachi-grid">
+        {diaChiDo.map((item) => (
+          <DiaChiCard
+            key={item.id}
+            item={item}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default DiaChiList;
