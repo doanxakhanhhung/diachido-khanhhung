c92ba5d94ae8f8c92d83b294e3cf66509fa4fc1d
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:20:46 +0700

    Create BanDo.jsx

---
+++ src/pages/BanDo.jsx
@@ -0,0 +1,11 @@
+function BanDo() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>🗺️ Bản đồ</h1>
+
+      <p>Bản đồ các địa chỉ đỏ sẽ hiển thị tại đây.</p>
+    </div>
+  );
+}
+
+export default BanDo;
