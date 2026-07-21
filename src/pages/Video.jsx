d707d44b67e8da1cb52a9e27bce5c13d20f43fa0
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:20:52 +0700

    Create Video.jsx

---
+++ src/pages/Video.jsx
@@ -0,0 +1,11 @@
+function Video() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>🎬 Video</h1>
+
+      <p>Danh sách video giới thiệu.</p>
+    </div>
+  );
+}
+
+export default Video;
