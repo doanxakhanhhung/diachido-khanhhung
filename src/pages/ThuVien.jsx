8e3b5a1e55c8d7ff1f1b57b32bb20d4ea53d48ab
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:20:50 +0700

    Create ThuVien.jsx

---
+++ src/pages/ThuVien.jsx
@@ -0,0 +1,11 @@
+function ThuVien() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>🖼️ Thư viện</h1>
+
+      <p>Ảnh và video của địa chỉ đỏ.</p>
+    </div>
+  );
+}
+
+export default ThuVien;
