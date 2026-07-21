5a4a2e7314333b070475c5337e0eb55ffda718f2
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:20:48 +0700

    Create DiaChi.jsx

---
+++ src/pages/DiaChi.jsx
@@ -0,0 +1,11 @@
+function DiaChi() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>📍 Địa chỉ đỏ</h1>
+
+      <p>Danh sách các địa chỉ đỏ của xã Khánh Hưng.</p>
+    </div>
+  );
+}
+
+export default DiaChi;
