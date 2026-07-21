bdbc43096bec2dbafb9a978fe4d82a4786ac3f93
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-21 21:20:39 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import DiaChi from "./pages/DiaChi";
+import BanDo from "./pages/BanDo";
+import ThuVien from "./pages/ThuVien";
+import Video from "./pages/Video";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dia-chi" element={<DiaChi />} />
+      <Route path="/ban-do" element={<BanDo />} />
+      <Route path="/thu-vien" element={<ThuVien />} />
+      <Route path="/video" element={<Video />} />
+    </Routes>
+  );
 }
 
 export default App;
