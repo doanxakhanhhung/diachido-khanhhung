f8aa5ba2ade537fc37a61fcc8be461a7b7c32847
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-24 19:54:53 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Stats from "../components/Stats";
 import Menu from "../components/Menu";
 import DiaChiList from "../components/DiaChiList";
 import Footer from "../components/Footer";
