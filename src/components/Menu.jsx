47165e3c872e195214bab019f3c2adc86b1e9f53
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-24 20:26:11 +0700

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,43 +1,31 @@
-import { Link } from "react-router-dom";
-
 function Menu() {
   return (
     <section className="menu">
 
-      <Link to="/" className="card-link">
-        <div className="card">
-          <h2>🏠</h2>
-          <h3>Trang chủ</h3>
-        </div>
-      </Link>
+      <div className="card">
+        <div className="icon">🏠</div>
+        <h3>Trang chủ</h3>
+      </div>
 
-      <Link to="/dia-chi" className="card-link">
-        <div className="card">
-          <h2>📍</h2>
-          <h3>Địa chỉ đỏ</h3>
-        </div>
-      </Link>
+      <div className="card">
+        <div className="icon">📍</div>
+        <h3>Địa chỉ đỏ</h3>
+      </div>
 
-      <Link to="/ban-do" className="card-link">
-        <div className="card">
-          <h2>🗺</h2>
-          <h3>Bản đồ</h3>
-        </div>
-      </Link>
+      <div className="card">
+        <div className="icon">🗺️</div>
+        <h3>Bản đồ</h3>
+      </div>
 
-      <Link to="/thu-vien" className="card-link">
-        <div className="card">
-          <h2>🖼</h2>
-          <h3>Thư viện</h3>
-        </div>
-      </Link>
+      <div className="card">
+        <div className="icon">🖼️</div>
+        <h3>Thư viện</h3>
+      </div>
 
-      <Link to="/video" className="card-link">
-        <div className="card">
-          <h2>🎥</h2>
-          <h3>Video</h3>
-        </div>
-      </Link>
+      <div className="card">
+        <div className="icon">🎥</div>
+        <h3>Video</h3>
+      </div>
 
     </section>
   );
