50ada867909256b3783a18e3435379292e2fda82
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-24 19:54:59 +0700

    Create Stats.jsx

---
+++ src/components/Stats.jsx
@@ -0,0 +1,32 @@
+function Stats() {
+  return (
+    <section
+  className="stats"
+  data-aos="fade-up"
+>
+
+      <div className="stat">
+        <h2>03</h2>
+        <p>Địa chỉ đỏ</p>
+      </div>
+
+      <div className="stat">
+        <h2>50+</h2>
+        <p>Hình ảnh</p>
+      </div>
+
+      <div className="stat">
+        <h2>10+</h2>
+        <p>Video</p>
+      </div>
+
+      <div className="stat">
+        <h2>100%</h2>
+        <p>Dữ liệu số hóa</p>
+      </div>
+
+    </section>
+  );
+}
+
+export default Stats;
