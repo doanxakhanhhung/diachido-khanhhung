c0defd17372c3854dcbd48663b143a97573c9261
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-25 19:34:43 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,50 @@
+import { useEffect, useState } from "react";
 import logo from "../assets/images/logo-doan.png";
 
 function Header() {
+
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+
+      if (window.scrollY > 80) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  }, []);
+
   return (
-    <header className="header">
+
+    <header className={active ? "header active" : "header"}>
 
       <div className="header-container">
 
         <div className="logo">
-
           <img src={logo} alt="Logo Đoàn" />
-
         </div>
 
         <div className="title">
-
           <h2>ĐOÀN TNCS HỒ CHÍ MINH</h2>
-
           <h3>XÃ KHÁNH HƯNG</h3>
-
           <p>TỈNH TÂY NINH</p>
-
         </div>
 
       </div>
 
     </header>
+
   );
+
 }
 
 export default Header;
