c258cbfed74229175e43940f88f3f8047a6ca5ec
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-25 19:42:49 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,7 @@ import Stats from "../components/Stats";
 import Menu from "../components/Menu";
 import DiaChiList from "../components/DiaChiList";
 import Footer from "../components/Footer";
-
 import "../css/style.css";
-import Stats from "../components/Stats";
 
 function Home() {
   return (
