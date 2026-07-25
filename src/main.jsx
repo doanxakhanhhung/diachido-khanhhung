0a12dc8afa41de7daf30e145f6e03b1746ec4921
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-25 20:25:27 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration:1000,
+  once:true,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
