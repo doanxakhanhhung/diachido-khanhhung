fe5fe17eab1b9fe61ff5d49b3b719dfe22b01163
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-25 20:25:24 +0700

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,22 +2,34 @@ function Stats() {
   return (
     <section className="stats">
 
-      <div className="stat">
+      <div
+  className="stat"
+  data-aos="fade-up"
+>
         <h2>03</h2>
         <p>Địa chỉ đỏ</p>
       </div>
 
-      <div className="stat">
+      <div
+  className="stat"
+  data-aos="fade-up"
+>
         <h2>100+</h2>
         <p>Hình ảnh tư liệu</p>
       </div>
 
-      <div className="stat">
+      <div
+  className="stat"
+  data-aos="fade-up"
+>
         <h2>10+</h2>
         <p>Video giới thiệu</p>
       </div>
 
-      <div className="stat">
+      <div
+  className="stat"
+  data-aos="fade-up"
+>
         <h2>24/7</h2>
         <p>Truy cập mọi lúc</p>
       </div>
