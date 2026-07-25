270f554e802f8ddd10cf330627704bed08417c96
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-25 20:25:22 +0700

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,27 +2,42 @@ function Menu() {
   return (
     <section className="menu">
 
-      <div className="card">
+      <div
+   className="card"
+   data-aos="zoom-in"
+>
         <div className="icon">🏠</div>
         <h3>Trang chủ</h3>
       </div>
 
-      <div className="card">
+      <div
+   className="card"
+   data-aos="zoom-in"
+>
         <div className="icon">📍</div>
         <h3>Địa chỉ đỏ</h3>
       </div>
 
-      <div className="card">
+      <div
+   className="card"
+   data-aos="zoom-in"
+>
         <div className="icon">🗺️</div>
         <h3>Bản đồ</h3>
       </div>
 
-      <div className="card">
+      <div
+   className="card"
+   data-aos="zoom-in"
+>
         <div className="icon">🖼️</div>
         <h3>Thư viện</h3>
       </div>
 
-      <div className="card">
+      <div
+   className="card"
+   data-aos="zoom-in"
+>
         <div className="icon">🎥</div>
         <h3>Video</h3>
       </div>
