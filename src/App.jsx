8c4c9f298f06a9cb166f61b0eecdae0357b8a6f2
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-07-29 16:51:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import Home from "./pages/Home";
 import DiaChi from "./pages/DiaChi";
@@ -6,34 +10,57 @@ import BanDo from "./pages/BanDo";
 import ThuVien from "./pages/ThuVien";
 import Video from "./pages/Video";
 import ChiTiet from "./pages/ChiTiet";
-import { useEffect } from "react";
 
-import AOS from "aos";
 function App() {
 
   useEffect(() => {
 
     AOS.init({
-
-      duration:1000,
-
-      once:true,
-
-      offset:100
-
+      duration: 1000,
+      once: true,
+      offset: 100,
     });
 
-  },[]);
+  }, []);
+
   return (
+
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dia-chi" element={<DiaChi />} />
-      <Route path="/ban-do" element={<BanDo />} />
-      <Route path="/thu-vien" element={<ThuVien />} />
-      <Route path="/video" element={<Video />} />
-      <Route path="/chi-tiet/:id" element={<ChiTiet />} />
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/dia-chi"
+        element={<DiaChi />}
+      />
+
+      <Route
+        path="/ban-do"
+        element={<BanDo />}
+      />
+
+      <Route
+        path="/thu-vien"
+        element={<ThuVien />}
+      />
+
+      <Route
+        path="/video"
+        element={<Video />}
+      />
+
+      <Route
+        path="/chi-tiet/:id"
+        element={<ChiTiet />}
+      />
+
     </Routes>
+
   );
+
 }
 
 export default App;
