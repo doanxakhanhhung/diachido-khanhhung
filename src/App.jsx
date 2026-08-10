9a17c7add50ef36328782fe2c7e81e0274d79931
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-08-10 20:38:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
 import { useEffect } from "react";
 
 import AOS from "aos";
@@ -11,7 +16,38 @@ import ThuVien from "./pages/ThuVien";
 import Video from "./pages/Video";
 import ChiTiet from "./pages/ChiTiet";
 
+
+// ===========================
+// CUỘN LÊN ĐẦU TRANG
+// ===========================
+
+function ScrollToTop() {
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+
+  }, [pathname]);
+
+  return null;
+}
+
+
+// ===========================
+// APP
+// ===========================
+
 function App() {
+
+  // ===========================
+  // AOS
+  // ===========================
 
   useEffect(() => {
 
@@ -23,41 +59,54 @@ function App() {
 
   }, []);
 
+
   return (
 
-    <Routes>
+    <>
 
-      <Route
-        path="/"
-        element={<Home />}
-      />
+      {/* Tự động đưa trang lên đầu khi chuyển trang */}
+      <ScrollToTop />
 
-      <Route
-        path="/dia-chi"
-        element={<DiaChi />}
-      />
 
-      <Route
-        path="/ban-do"
-        element={<BanDo />}
-      />
+      {/* ===========================
+          ROUTES
+      =========================== */}
 
-      <Route
-        path="/thu-vien"
-        element={<ThuVien />}
-      />
+      <Routes>
 
-      <Route
-        path="/video"
-        element={<Video />}
-      />
+        <Route
+          path="/"
+          element={<Home />}
+        />
 
-      <Route
-        path="/chi-tiet/:id"
-        element={<ChiTiet />}
-      />
+        <Route
+          path="/dia-chi"
+          element={<DiaChi />}
+        />
 
-    </Routes>
+        <Route
+          path="/ban-do"
+          element={<BanDo />}
+        />
+
+        <Route
+          path="/thu-vien"
+          element={<ThuVien />}
+        />
+
+        <Route
+          path="/video"
+          element={<Video />}
+        />
+
+        <Route
+          path="/chi-tiet/:id"
+          element={<ChiTiet />}
+        />
+
+      </Routes>
+
+    </>
 
   );
 
