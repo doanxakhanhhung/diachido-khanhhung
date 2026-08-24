33d98900fcc3ff8cefbe6ebd0a3fd0b534889e8c
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-08-24 20:26:46 +0700

    Update diaChiDo.js

--- src/data/diaChiDo.js
+++ src/data/diaChiDo.js
@@ -42,9 +42,7 @@ const diaChiDo = [
 
     map: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d426.0167684097204!2d105.69563403218842!3d10.951191574625849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1svi!2s!4v1784813782335!5m2!1svi!2s",
 
-    video: "https://youtube.com",
-
-   
+       
     hinhanh: goChauMaiBanner,
 
     gallery: [
@@ -75,9 +73,7 @@ const diaChiDo = [
 
     map: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d668.9282569733205!2d105.74002299833792!3d11.004693734096886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1svi!2s!4v1784813579237!5m2!1svi!2s",
 
-    video: "https://youtube.com",
-
-   
+      
     hinhanh: benPhoBanner,
 
     gallery: [
@@ -108,9 +104,7 @@ const diaChiDo = [
 
     map: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d569.3391826023246!2d105.81207381559021!3d10.932089946735276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1svi!2s!4v1784813722435!5m2!1svi!2s",
 
-    video: "https://youtube.com",
-
-    
+       
     hinhanh: thaiTriBanner,
 
     gallery: [
