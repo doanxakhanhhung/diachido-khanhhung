1b3bb75f3c7c343cb0e68247c7ab18b1254fbbc2
doanxakhanhhung <truonghuutai1993@gmail.com>
2026-08-24 16:01:40 +0700

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <section className="about">
+      <div className="about-container">
+        <h2>Địa chỉ đỏ xã Khánh Hưng là nơi lưu giữ những dấu ấn lịch sử, truyền thống cách mạng và những giá trị thiêng liêng của quê hương. Đây là không gian để thế hệ hôm nay tìm hiểu, tưởng nhớ và tri ân các anh hùng liệt sĩ, những người đã cống hiến, hy sinh vì độc lập, tự do của Tổ quốc. Thông qua việc số hóa và giới thiệu các địa chỉ đỏ, hình ảnh, tư liệu lịch sử, website góp phần giáo dục truyền thống yêu nước, bồi đắp niềm tự hào quê hương và lan tỏa những giá trị lịch sử đến đoàn viên, thanh thiếu nhi và Nhân dân xã Khánh Hưng.</h2>
+      </div>
+    </section>
+  );
+}
+
+export default About;
